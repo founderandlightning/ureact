57fd46713ac99f11da3ca06d29f6e77e3eb54508
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2020-06-25 15:54:37 +0530

    remove the errorhandle file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
-export { apiUrl, env } from "./env";
-export { history } from "./history";
-export { cookies } from "./cookies";
-export * from "./DataValidation";
-export { serverErrors } from './handleClientError';
+export { apiUrl, env } from './env';
+export { history } from './history';
+export { cookies } from './cookies';
+export * from './DataValidation';
